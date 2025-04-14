cce60b69efb13d7454b9a1fbbc093122caebb259
rabbitCase <gnair3004@gmail.com>
2025-04-14 22:45:18 +0530

    blur and focus effect

---
+++ frontend/home/main.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+    document.getElementById('headers').style.filter='none';
+    document.getElementById('button').style.filter='none';
+    document.getElementById('headers').style.transform = 'translateY(20px)';
+},100);
