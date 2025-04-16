da0c5ec8d1c87d1214a67ed91138b34653d93cf4
rabbitCase <gnair3004@gmail.com>
2025-04-16 13:42:38 +0530

    updated server logic

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 require('dotenv').config();
+const registeredstaff = require('./registeredStaff.js');
+const allowedstaff = require('./allowedStaff.js');
+
 const cors = require('cors');
 const path = require('path');
 const express = require('express');
@@ -34,6 +37,8 @@ app.use('/welcome',welcomerouter);
 app.use('/login',staffloginrouter);
 app.use('/logged-in',loggedinrouter);
 
+let loginName = "";
+
 app.get('/', (req, res) => {
     app.use(express.static(path.join(__dirname,"../frontend/home")));
     res.sendFile(path.join(__dirname,"../frontend/home/index.html"));
@@ -41,18 +46,22 @@ app.get('/', (req, res) => {
 
 app.post('/welcome', (req,res) =>{
     let query = `select AirlineID, DepartureAirportID, ArrivalAirportID, TIME(DepartureTime) as DepartureTime from flight where DepartureAirportID = ${req.body.depairport} and ArrivalAirportID = ${req.body.arrairport} and DATE(DepartureTime) = '${req.body.tripdate}'`;
+
     dbConnection.query(query, (err,result) => {
         if(err){
             console.log("Error occured: ", err);
         }
         res.json(result);
+        console.log(result);
     });
 });
 
 app.post('/delay', (req,res) => {
     let flightid = req.body.flightid;
     let delay = req.body.delay;
+
     const query = `update flight set delay = ${delay} where FlightID = ${flightid}`;
+
     dbConnection.query(query, (err,result) =>{
         if (err) {
             console.log("Query error:", err);
@@ -64,15 +73,112 @@ app.post('/delay', (req,res) => {
 });
 
 app.post('/baggage', (req,res) => {
-    res.json({status : "received", data : req.body});
+    let passengerid = req.body.passengerid;
+    let baggagetag = req.body.baggagetag;
+    let weight = req.body.weight;
+    let departureairport = req.body.departureairport;
+    let arrivalairport = req.body.arrivalairport;
+
+    const query = `insert into baggage values(${passengerid},'${baggagetag}',${weight},'${departureairport}','${arrivalairport}')`;
+
+    dbConnection.query(query, (err,result) =>{
+        if (err) {
+            console.log("Query error:", err);
+            return res.json({ error: "Query failed", details : err });
+        }
+        res.json({message : "query executed"});
+        console.log("Query result:", result);
+    });
 });
 
 app.post('/lostandfound', (req,res) => {
-    res.json({status : "received", data : req.body});
+    let itemid = req.body.itemid;
+    let airportid = req.body.airportid;
+    let datefound = req.body.datefound;
+    let description = req.body.description;
+
+    const query = `insert into lostandfound values(${itemid}, ${airportid}, '${datefound}', '${description}')`;
+
+    dbConnection.query(query, (err,result) =>{
+        if (err) {
+            console.log("Query error:", err);
+            return res.json({ error: "Query failed", details : err });
+        }
+        res.json({message : "query executed"});
+        console.log("Query result:", result);
+    });
 });
 
 app.post('/register', (req,res) => {
-    res.json({status : "received", data : req.body});
+    let staffname = req.body.staffname;
+    let staffid = req.body.staffid;
+    let airportid = req.body.airportid;
+    let role = req.body.role;
+    
+    const queryStaff = `insert into staff values(${staffid}, ${airportid}, '${staffname}')`;
+    const queryRole = `insert into staff_role values(${staffid}, '${role}')`;
+    const queryWorksFor = `insert into works_for values(${staffid}, '${airportid}')`;
+    
+    if (!allowedstaff.mutableArrayObject.allowed.includes(Number(staffid))) {
+        return res.json({
+            message: "not allowed",
+            details: "user is not employed by the airport and/or the employment status is not updated in the database"
+        });
+    }
+
+    registeredstaff.mutableArrayObject.registered.push(Number(staffid));//add the registered staff to the registered array
+
+    console.log("Updated registered staff: ",registeredstaff.mutableArrayObject.registered);
+
+    allowedstaff.mutableArrayObject.allowed = allowedstaff.mutableArrayObject.allowed.filter(value => value != Number(staffid));//remove this staff from the allowed array
+
+    console.log("Updated allowed staff: ",allowedstaff.mutableArrayObject.allowed);
+
+    dbConnection.query(queryStaff, (err) => {
+        if (err) {
+            console.log("Query error:", err);
+            return res.json({ error: "Query failed", details: err });
+        }
+
+        dbConnection.query(queryRole, (err) => {
+            if (err) {
+                console.log("Query error:", err);
+                return res.json({ error: "Query failed", details: err });
+            }
+
+            dbConnection.query(queryWorksFor, (err) => {
+                if (err) {
+                    console.log("Query error:", err);
+                    return res.json({ error: "Query failed", details: err });
+                }
+                res.json({ message: "queries executed" });
+            });
+        });
+    });
+   
+});
+
+app.post('/login', (req,res) => {
+    loginName = "";
+    const query = `select name from staff where staffid = ${req.body.staffid}`;
+    dbConnection.query(query, (err,result) => {
+        if(err){
+            console.log("Query error:", err);
+            return res.json({ error: "Query failed", details: err });
+        }
+        loginName = result;
+    });
+
+    if(registeredstaff.mutableArrayObject.registered.includes(Number(req.body.staffid))){
+        res.send({message : "authenticated"});
+    }
+    else{
+        res.send({message : "not allowed"});
+    }
+});
+
+app.get('/getname', (req,res) => {
+    res.json({message : loginName[0].name});
 });
 
 app.listen(port, () => {
