97d7fefa6c9cbffe317a647f10f758ec78991e00
rabbitCase <gnair3004@gmail.com>
2025-04-18 01:40:24 +0530

    tools used svgs added and alignment issued fixed

--- frontend/home/main.js
+++ frontend/home/main.js
@@ -1,5 +1,6 @@
 setTimeout(() => {
     document.getElementById('headers').style.filter='none';
     document.getElementById('button').style.filter='none';
+    document.getElementById('icon-container').style.filter='none';
     document.getElementById('headers').style.transform = 'translateY(20px)';
 },100);
